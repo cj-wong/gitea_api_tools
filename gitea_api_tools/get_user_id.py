bdac05ba680f7f3a65b3998e58634bb4ef5a6d78
CJ Wong <cj-wong@users.noreply.github.com>
2023-06-14 04:34:48 -0700

    Ensure the user has a choice in getting user ID if already configured
    The user can now cancel the request if a user ID is already configured
    and not overwrite the configured value if it differs. The script will
    also not write to configuration if the user ID is the same as before.

--- gitea_api_tools/get_user_id.py
+++ gitea_api_tools/get_user_id.py
@@ -28,11 +28,31 @@ def get_user_id() -> int:
 
 def main() -> None:
     """Get the user ID and optionally store it into the configuration."""
-    user_id = get_user_id()
-    config.logger.info(f"Your user ID is {user_id}.")
+    old_uid = getattr(config.config, 'uid')
+    if old_uid:
+        config.logger.warning(
+            f"Your user ID ({old_uid}) has already been configured.")
+        choice = input("Would you still like to proceed? [yN] ")
+        if not choice.lower().startswith('y'):
+            config.logger.info("Stopped.")
+            return
+
+    new_uid = get_user_id()
+    config.logger.info(f"Your user ID is {new_uid}.")
+    if old_uid and new_uid != old_uid:
+        config.logger.warning(
+            "This new user ID differs from the one stored in configuration.")
+        choice = input("Would you still like to proceed? [yN] ")
+        if not choice.lower().startswith('y'):
+            config.logger.info("Stopped.")
+            return
+    elif old_uid:
+        config.logger.info("This user ID is identical; no action is needed.")
+        return
+
     choice = input("Would you like to store this in the configuration? [yN] ")
     if choice.lower().startswith('y'):
-        setattr(config.config, 'uid', user_id)
+        setattr(config.config, 'uid', new_uid)
         config.config.write_config()
         config.logger.info("User ID was stored in config.json.")
     else:
