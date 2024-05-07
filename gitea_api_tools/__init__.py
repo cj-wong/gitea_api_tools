74e494af2266b7315005270b102ae07bbcc11cb9
CJ Wong <cj-wong@users.noreply.github.com>
2024-05-07 00:25:28 -0700

    Fix aliases not running any commands
    Currently, `dest` in a sub-parser set is assigned to the sub-parser
    that gets called. This also applies to aliases. When I used the base
    sub-parser as the match condition, aliases just don't get matched as a
    result. Now, I've abandoned the `dest` approach in favor of the
    (officially) recommended solution: `set_defaults()` for setting the
    function. However, as a consequence of the varying function signatures,
    I have to add wrapper functions that use the arguments.

--- gitea_api_tools/__init__.py
+++ gitea_api_tools/__init__.py
@@ -6,19 +6,41 @@ from .config import configure
 from .package import version
 
 
+# These functions serve purely as wrappers for the sub-commands' function.
+def wrap_subparser_configure(args: argparse.Namespace) -> None:
+    configure.configure_interactively()
+
+
+def wrap_subparser_get_deploykeys(args: argparse.Namespace) -> None:
+    gitea.repo.deploy_key.get_keyed_repos()
+
+
+def wrap_subparser_get_uid(args: argparse.Namespace) -> None:
+    gitea.user.store_retrieved_id()
+
+
+def wrap_subparser_list_python(args: argparse.Namespace) -> None:
+    package.python.list_dependent_repos(args.package, args.version)
+
+
 parser = argparse.ArgumentParser(description="A toolbox for Gitea API")
-subparsers = parser.add_subparsers(required=True, dest="command")
+subparsers = parser.add_subparsers(required=True)
 
 # Sub-commands that take no arguments
 parser_configure = subparsers.add_parser("configure")
+parser_configure.set_defaults(func=wrap_subparser_configure)
+
 parser_deploy_keys = subparsers.add_parser(
     "deploy_keys",
     aliases=["dep", "keys", "dk"],
     description="View deploy keys",
 )
+parser_deploy_keys.set_defaults(func=wrap_subparser_get_deploykeys)
+
 parser_user_id = subparsers.add_parser(
     "user_id", aliases=["uid", "id", "whoami"], description="View your user ID"
 )
+parser_user_id.set_defaults(func=wrap_subparser_get_uid)
 
 # Sub-commands that require at least one argument
 parser_python = subparsers.add_parser(
@@ -34,22 +56,21 @@ parser_python.add_argument(
     default=version.SENTINEL_VERSION,
     help="optional version string like 1.0.0; don't prefix with 'v'",
 )
+parser_python.set_defaults(func=wrap_subparser_list_python)
 
 
 def main() -> None:
-    """Run the Gitea API toolkit."""
+    """Run the Gitea API toolkit.
+
+    Raises:
+        RuntimeError: invalid option from command line
+
+    """
     args = parser.parse_args()
-    match args.command:
-        case "configure":
-            configure.configure_interactively()
-        case "deploy_keys":
-            gitea.repo.deploy_key.get_keyed_repos()
-        case "user_id":
-            gitea.user.store_retrieved_id()
-        case "python":
-            package.python.list_dependent_repos(args.package, args.version)
-        case _:
-            raise RuntimeError("Invalid option provided")
+    try:
+        args.func(args)
+    except AttributeError:
+        raise RuntimeError("Invalid option provided")
 
 
 if __name__ == "__main__":
